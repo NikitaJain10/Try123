0f9d8dd32ded55e2113c17f13ace5d01d371a056
KUMAR DIVIJ <divijsinha23@gmail.com>
2025-04-09 11:59:13 +0530

    ques 1 explanation

--- test/test/obj/Debug/net8.0/test.AssemblyInfo.cs
+++ test/test/obj/Debug/net8.0/test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f59cebb30204aca96824a5487c681b0c2cb5ca2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f956dc09c5627ce32eb4f9c9426a49b9707e34f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
